6a750ca24f91444c7d7e6c782f1f938a1fbbf7e0
Renan Carneiro <renanvy@gmail.com>
2019-12-02 21:04:19 -0300

    Adiciona mutation para criar um novo usuário

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,11 +4,13 @@ import { GraphQLServer } from 'graphql-yoga'
 const USERS = [
   {
     id: 1,
-    name: 'John'
+    name: 'John',
+    email: 'john@gmail.com'
   },
   {
     id: 2,
-    name: 'Mary'
+    name: 'Mary',
+    email: 'mary@gmail.com'
   }
 ]
 
@@ -16,6 +18,19 @@ const typeDefs = resolve(__dirname, 'schema.graphql')
 const resolvers = {
   Query: {
     users: () => USERS
+  },
+  Mutation: {
+    createUser: (parent, args, ctx, info) => {
+      const { data } = args
+      const user = {
+        ...data,
+        id: USERS.length + 1
+      }
+
+      USERS.push(user)
+      
+      return user
+    }
   }
 }
 
